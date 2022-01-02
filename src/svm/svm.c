9f1f8ba9d4aaa579dd798ffe4aff7b60fd181d44
iinsertNameHere <iinsert.mailhere@gmail.com>
2022-01-02 21:17:36 +0100

    Cleaned up CLI parsing for svm

--- src/svm/svm.c
+++ src/svm/svm.c
@@ -8,23 +8,58 @@
 // Slick VM
 SVM svm = {0};
 
-//TODO: Watch https://www.youtube.com/watch?v=9td67NTtNCg&list=PLpM-Dvs8t0VY73ytTCQqgvgCWttV3m8LM&index=4 at 1:10:02.
 //NOTE: GitHub https://github.com/iinsertNameHere/Slick
+
+void usage(FILE* stream)
+{
+    fprintf(stream, "Usage: svm -i <input.sbc> [options]\n");
+    fprintf(stream, "  -h          Provides a help list.\n");
+    fprintf(stream, "  -l <limit>  Sets a execution limit.\n");
+}
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "ERROR: Expected input!\n");
-        fprintf(stderr, "Usage: svm <input.sbc>\n");
+    shift(&argc, &argv); // Skip program name.
+    char* inputFilePath = NULL;
+    int limit = -1;
+
+    while (argc > 0) {
+        const char* flag = shift(&argc, &argv);
+        if (strcmp(flag, "-i") == 0) {
+            if (argc == 0) {
+                fprintf(stderr, "ERROR: No argument is provided for flag '%s'\n", flag);
+                usage(stderr);
+                exit(1);
+            }
+            inputFilePath = shift(&argc, &argv);
+        } else if (strcmp(flag, "-l") == 0) {
+            if (argc == 0) {
+                fprintf(stderr, "ERROR: No argument is provided for flag '%s'\n", flag);
+                usage(stderr);
+                exit(1);
+            }
+            limit = atoi(shift(&argc, &argv));
+        } else if (strcmp(flag, "-h") == 0) {
+            usage(stdout);
+            exit(0);
+        } else {
+            fprintf(stderr, "ERROR: Unknown flag '%s'\n", flag);
+            usage(stderr);
+            exit(1);
+        }
+    }
+
+    if (inputFilePath == NULL) {
+        fprintf(stderr, "ERROR: Expected input file!\n");
+        usage(stderr);
         exit(1);
     }
 
-    svm_loadProgramFromFile(&svm, argv[1]);
-    ExeptionState state = svm_execProgram(&svm, 69);
+    svm_loadProgramFromFile(&svm, inputFilePath);
+    ExeptionState state = svm_execProgram(&svm, limit);
     if (state != EXEPTION_STACK_OVERFLOW) {
         svm_dumpStack(stdout, &svm);
-    }
-    if (state != EXEPTION_SATE_OK) {
+    } else if (state != EXEPTION_SATE_OK) {
         fprintf(stderr, "ERROR: Failed to execute program! : %s\n", exeption_as_cstr(state));
         return 1;
     }
