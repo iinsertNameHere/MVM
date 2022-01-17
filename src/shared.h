a30cd8602059d1d0bef5b33112aef610ba83febc
iinsertNameHere <iinsert.mailhere@gmail.com>
2022-01-17 22:28:23 +0100

    Update shared.h

--- src/shared.h
+++ src/shared.h
@@ -569,7 +569,7 @@ ExeptionState mvm_execInst(MVM* mvm)
             if (mvm->stack_size < 1) {
                 return EXEPTION_STACK_UNDERFLOW;
             }
-            mvm->stack[mvm->stack_size - 2].as_u64 = mvm->stack[mvm->stack_size - 2].as_u64 >> mvm->stack[mvm->stack_size - 1].as_u64;
+            mvm->stack[mvm->stack_size - 1].as_u64 = ~mvm->stack[mvm->stack_size - 1].as_u64;
             mvm->stack_size -= 1;
             mvm->ip += 1;
             break;
@@ -579,7 +579,8 @@ ExeptionState mvm_execInst(MVM* mvm)
             if (mvm->stack_size < 2) {
                 return EXEPTION_STACK_UNDERFLOW;
             }
-            mvm->stack[mvm->stack_size - 1].as_u64 = ~mvm->stack[mvm->stack_size - 1].as_u64;
+            mvm->stack[mvm->stack_size - 2].as_u64 = mvm->stack[mvm->stack_size - 2].as_u64 >> mvm->stack[mvm->stack_size - 1].as_u64;
+            mvm->stack_size -= 1;
             mvm->ip += 1;
             break;
         }
